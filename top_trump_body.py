8646ce1c5094c4269cb0b9f061cb5f52f4e79029
areilly14 <mr.aidan.reilly@gmail.com>
2024-09-15 15:14:31 +0100

    Fixed issues when dealing cards

--- top_trump_body.py
+++ top_trump_body.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+#Generate the cards with random values in each category between 1 and 100
 def makeCards():
     cards =pd.read_csv('country.csv')
     numberOfCards = len(cards)
@@ -9,17 +10,18 @@ def makeCards():
     cards['Happiness'] = np.random.randint(100, size=numberOfCards)
     cards['Owner'] = np.zeros(numberOfCards)
     cards['Position'] = range(numberOfCards)
-    print(cards.to_string())
     return cards
 
+#Deal cards into two piles
 def dealCards():
     for index, row in cards.iterrows():
         if row['Position']%2 ==0:
-            row['Owner'] = 1
+            cards.at[index,'Owner'] = 1
         else:
-            row['Owner'] = 2
-        row['Position'] = row['Position']//2
-
+            cards.at[index,'Owner'] = 2
+        cards.at[index,'Position'] = row['Position']//2
+        
+#Generate and deal cards out
 cards = makeCards()
 dealCards()
 print(cards.to_string())
