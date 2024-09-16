3bcfe640a65cd55d259690f4de24cf847700e95b
areilly14 <mr.aidan.reilly@gmail.com>
2024-09-16 17:19:11 +0100

    Passing the cards to belong to the winner of the round and shifting the positions

--- top_trump_body.py
+++ top_trump_body.py
@@ -47,17 +47,26 @@ def playRound(chooser):
     print("Player "+str(winner) +" won this round")
     print("The cards in play were...")
     print(top_cards.to_string())
+    winner_number_of_cards = len(cards[cards['Owner'] == winner])
+    for i in range(len(top_cards)):
+        top_cards.at[i,'Owner'] = winner
+        top_cards.at[i,'Position'] = winner_number_of_cards + i
+    cards.update(top_cards)
+    cards["Position"] = cards["Position"] - 1
+    return winner
 
 def compareCards(chosen_category,top_cards,chooser):
-    winningCard = top_cards[cards['Owner'] == chooser]
-    column_names = cards.columns
-    category_index = 1
-    winningCard = top_cards.loc[top_cards[chosen_category].idxmax()]
-    return winningCard['Owner']
+    winning_card = top_cards.loc[top_cards[chosen_category].idxmax()]
+    return winning_card['Owner']
         
 #Generate and deal cards out
 cards = makeCards()
 number_of_players = dealCards()
 cards = sortCards()
 #print(cards.to_string())
-playRound(1)
+current_winner = 1
+while len(cards)>len(cards[cards['Owner']==1])>0:
+    current_winner = playRound(current_winner)
+    cards = sortCards()
+    print("You now have "+ str(len(cards[cards['Owner']==1]))+ " cards")
+    input("Ready to start next round?")
